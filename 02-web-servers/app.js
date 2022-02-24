dadb01dad2fb7a81b54ff0f1a39f4d777f8e14f9
Luz Sanchez <nena_baby23@hotmail.com>
2022-02-23 20:57:44 -0700

    finishing hw'

--- 02-web-servers/app.js
+++ 02-web-servers/app.js
@@ -2,6 +2,16 @@ const http = require('http')
 const fs = require('fs')
 var PORT = 7500;
 
+var quotes = [
+    `Be the Reason Someone Smile Today`,
+    `Never Stop Dreaming`,
+    `Life is Beautiful`,
+    `Everyday is a New Day`,
+    `Make Happiness a Habit`
+
+  ]
+  var display = quotes[Math.floor(Math.random() * quotes.length)]
+  
 const server = http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' })
     fs.readFile('second.html', function (error, data) {
@@ -12,7 +22,7 @@ const server = http.createServer(function (req, res) {
             res.write(data)
         }
 
-        res.end()
+        res.end(` ${display}`)
     })
 })
 
--- 02-web-servers/server.js
+++ 02-web-servers/server.js
@@ -2,23 +2,31 @@ const http = require('http')
 const fs = require('fs')
 var PORT = 7000;
 
-const server = http.createServer(function(req, res) {
-res.writeHead(200, { 'Content-Type': 'text/html'})
-fs.readFile('index.html', function(error, data) {
-  if (error) {
-    res.writeHead(404)
-    res.write('Error: File not Found')
-  } else {
-    res.write(data)
-  }
+var quotes = [
+  `Believe you can and you are halfway there`,
+  `Dont stop until you are proud`,
+  `Do good and good will come to you`,
+  `The purpose of life is to be happy`
+]
+var display = quotes[Math.floor(Math.random() * quotes.length)]
 
-res.end()
-})
+const server = http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/html' })
+  fs.readFile('index.html', function (error, data) {
+    if (error) {
+      res.writeHead(404)
+      res.write('Error: File not Found')
+    } else {
+      res.write(data)
+    }
+
+    res.end(` ${display}`)
+  })
 })
 
-server.listen(PORT, function(error) {
+server.listen(PORT, function (error) {
   if (error) {
-    console.log ('no bueno' ,error)
+    console.log('no bueno', error)
   } else {
     console.log('si bueno' + PORT)
   }
